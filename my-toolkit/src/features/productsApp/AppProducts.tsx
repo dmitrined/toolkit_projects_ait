e754d86055b909c948827b6ae24d4f1df0a61f46
dmitrined <dm.nedioglo@gmail.com>
2025-11-23 11:01:54 +0100

    ok

--- my-toolkit/src/features/productsApp/AppProducts.tsx
+++ my-toolkit/src/features/productsApp/AppProducts.tsx
@@ -7,6 +7,29 @@ export default function AppProducts(): JSX.Element{
     const [showCart, setShowCart] = useState(false);
   return (
     <div>
+       <div className="flex justify-center">
+      <a
+        className="
+          inline-block 
+          py-2 px-4            
+          text-white           
+          bg-gray-800          
+          border-2 border-gray-800 
+          rounded-lg           
+          font-bold            
+          text-base            
+          cursor-pointer       
+          mt-4                 
+          transition duration-300 
+          hover:bg-gray-700    
+          hover:border-gray-700
+        "
+        target="_blank"
+        href="https://github.com/dmitrined/toolkit_projects_ait/tree/main/my-toolkit/src/features/productsApp"
+      >
+        Посмотреть код этой страницы  GitHub
+      </a>
+    </div>
       <Products />
 <CartIcon onClick={() => setShowCart((prev) =>!prev)}/>
 {showCart && <Cart />}
